489a55b1dd169d094089084293170315d16b145e
Rajendra Kulkarni <frankly.coder@gmail.com>
2021-09-06 00:07:31 +0530

    Added mongo db database

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const port = 8000;
 
+const db = require('./config/mongoose');
+const Contact = require('./models/contact');
 const app = express();
 
 var contactList = [
