574c2633ee7d69e88e01075583a14a6142682cda
Rajendra Kulkarni <frankly.coder@gmail.com>
2021-09-06 23:24:19 +0530

    Added mongo db and all thins

--- index.js
+++ index.js
@@ -6,32 +6,32 @@ const db = require('./config/mongoose');
 const Contact = require('./models/contact');
 const app = express();
 
-var contactList = [
-    {
-        person : 'Raju',
-        phone : '123456789',
-    },
-    {
-        person : 'Raju1',
-        phone : '123456789',
-    },
-    {
-        person : 'Raju2',
-        phone : '123456789',
-    },
-    {
-        person : 'Raju3',
-        phone : '123456789',
-    },
-    {
-        person : 'Raju4',
-        phone : '123456789',
-    },
-    {
-        person : 'Raju5',
-        phone : '123456789',
-    }
-]
+// var contactList = [
+//     {
+//         person : 'Raju',
+//         phone : '123456789',
+//     },
+//     {
+//         person : 'Raju1',
+//         phone : '123456789',
+//     },
+//     {
+//         person : 'Raju2',
+//         phone : '123456789',
+//     },
+//     {
+//         person : 'Raju3',
+//         phone : '123456789',
+//     },
+//     {
+//         person : 'Raju4',
+//         phone : '123456789',
+//     },
+//     {
+//         person : 'Raju5',
+//         phone : '123456789',
+//     }
+// ]
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views') );
@@ -42,21 +42,45 @@ app.get('/', function(req, res){
     // console.log(__dirname);
     // res.send('<h1>Cool ! It is running</h1>');
 
-    return res.render('home', { 
-        title : 'My Contact List',
-        contact_list : contactList,
+    Contact.find({}, function(err, allContacts){
+        if(err){
+            console.log('Error fetching contacts : ', err);
+            return;
+        }
+
+        return res.render('home', { 
+            title : 'My Contact List',
+            contact_list : allContacts,
+            // contact_list : Contact,
+        });
+
     });
+
+    
 });
 
 
 app.get('/delete/', function(req, res){
+
+    //get id from the request parameter in url
+
+
     console.log(req.query);
-    let phone = req.query.phone;
+    let id = req.query.id;
 
-    let contactIndex = contactList.findIndex( contact => contact.phone == phone);
-    contactList.splice(contactIndex, 1);
+    //find contact in database using id and delete it
+    // let contactIndex = contactList.findIndex( contact => contact._id == id);
 
-    res.redirect('back');
+    Contact.findByIdAndDelete(id, function(err){
+        if(err){
+            console.log('error deleting the contact, Error : ', err);
+            return;
+        }
+        res.redirect('back');
+    });
+    // contactList.splice(contactIndex, 1);
+
+    // res.redirect('back');
 
 });
 
@@ -75,8 +99,21 @@ app.post('/add-contact', function(req, res){
     //     phone : req.body.phone,
     // });
     console.log(req.body);
-    contactList.push(req.body);
-    res.redirect('/');
+    // contactList.push(req.body);
+
+    Contact.create({
+        person : req.body.person,
+        phone : req.body.phone,
+    }, function(err, newContact){
+        if(err){
+            console.log('Error : ', err);
+        }
+        else{
+            console.log('newContact : ',newContact);
+        }
+        return;
+    });
+    return res.redirect('back');
     // console.log(req.body);
     // return;
     // return res.send(.toString());
--- models/contact.js
+++ models/contact.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const contactSchema = new mongoose.Schema({
-    name : {
+    person : {
         type : String,
         required : true,
     }, 
